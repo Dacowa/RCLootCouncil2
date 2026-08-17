a0a5f8cde5a9092b499202cfb836542e05a82f12
Dacowa <dannydawnbringer@gmail.com>
2026-08-17 09:38:10 +0200

    changes to priority sort

--- Modules/VotingFrame/VotingFrame.lua
+++ Modules/VotingFrame/VotingFrame.lua
@@ -80,8 +80,8 @@ function RCVotingFrame:OnInitialize()
 		{ name = L["Votes"],         DoCellUpdate = RCVotingFrame.SetCellVotes,    colName = "votes",    sortnext = "diff",     width = 50,       align = "CENTER", }, -- 10 Number of votes
 		{ name = L["Vote"],          DoCellUpdate = RCVotingFrame.SetCellVote,     colName = "vote",     sortnext = "votes",    width = 60,       align = "CENTER", }, -- 11 Vote button
 		{ name = L["Notes"],         DoCellUpdate = RCVotingFrame.SetCellNote,     colName = "note",     width = 50,            align = "CENTER", },            -- 12 Note icon
-		{ name = _G.ROLL,            DoCellUpdate = RCVotingFrame.SetCellRoll,     colName = "roll",     sortnext = "priority", width = 50,       align = "CENTER", }, -- 13 Roll
-		{ name = "Priority",         DoCellUpdate = RCVotingFrame.SetCellPriority, colName = "priority", defaultsort = 2,     sortnext = "roll",    comparesort = PrioritySort, width = 60, align = "CENTER", }, -- 14 Loot Priority (S2)
+		{ name = _G.ROLL,            DoCellUpdate = RCVotingFrame.SetCellRoll,     colName = "roll",     width = 50,       align = "CENTER", }, -- 13 Roll
+		{ name = "Priority",         DoCellUpdate = RCVotingFrame.SetCellPriority, colName = "priority", defaultsort = 2,     comparesort = PrioritySort, width = 60, align = "CENTER", }, -- 14 Loot Priority (S2)
 		-- { name = "",				DoCellUpdate = RCVotingFrame.SetCellCorruption, colName = "corruption", sortnext = 10, width = 30, align = "CENTER",},					-- 14 Corruption (Patch 8.3)
 	}
 	-- The actual table being worked on, new entries should be added to this table "tinsert(RCVotingFrame.scrollCols, data)"
@@ -1946,13 +1946,19 @@ function PrioritySort(table, rowa, rowb, sortbycol)
 	local a, b = table:GetRow(rowa), table:GetRow(rowb)
 	if not (a and b) then return false end
 
+	local function normalizeRollValue(value)
+		local numeric = tonumber(value)
+		if numeric ~= nil then return numeric end
+		return 0
+	end
+
 	local LootPriority = addon.Require "Data.LootPriority"
 	local prioA = (a and a.name and LootPriority:GetPriorityRoll(a.name)) or 0
 	local prioB = (b and b.name and LootPriority:GetPriorityRoll(b.name)) or 0
 
 	if prioA == prioB then
-		local rollA = (lootTable[session].candidates[a.name] and lootTable[session].candidates[a.name].roll) or 0
-		local rollB = (lootTable[session].candidates[b.name] and lootTable[session].candidates[b.name].roll) or 0
+		local rollA = normalizeRollValue((lootTable[session].candidates[a.name] and lootTable[session].candidates[a.name].roll) or 0)
+		local rollB = normalizeRollValue((lootTable[session].candidates[b.name] and lootTable[session].candidates[b.name].roll) or 0)
 		if column.sortnext then
 			local nextcol = table.cols[column.sortnext]
 			if nextcol and not nextcol.sort then
